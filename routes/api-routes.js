03f3e3d98d1a1102f3a4d9d2e557aeef686a2ee5
Ian Walston <ikwalston@comcast.net>
2019-01-31 13:12:01 -0500

    foobar

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,6 +12,7 @@
 */
 
 const db = require("../models");
+const Op = db.Sequelize.Op;
 
 module.exports = app => {
   //This route I made for test purposes. It inserts 4 rows of test data into our test table.
@@ -20,11 +21,11 @@ module.exports = app => {
   app.get("/api/product/:product", (req, res) => {
     const product = req.params.product;
     const parsedproduct = product.replace(/%20/g, " ");
-    db.CaliProducts.findAll({ where: { ProductName: parsedproduct } }).then(
-      data => {
-        res.render("index", {products: data});
-      }
-    );
+    db.CaliProducts.findAll({
+      where: { ProductName: { [Op.like]: parsedproduct + "%" } }
+    }).then(data => {
+      res.render("index", { products: data });
+    });
   });
 
   app.get("/api/ingredient/:ingredient", (req, res) => {
@@ -32,14 +33,14 @@ module.exports = app => {
     const parsedingredient = ingredient.replace(/%20/g, " ");
     db.CaliProducts.findAll({ where: { ChemicalName: parsedingredient } }).then(
       data => res.json(data)
-    )
+    );
   });
 
   app.get("/api/ingredientrate/:ingredient", (req, res) => {
     const ingredient = req.params.ingredient;
     const parsedingredient = ingredient.replace(/%20/g, " ");
-    db.IngredientRatings.findAll({ where: { ChemicalName: parsedingredient } }).then(
-      data => res.json(data)
-    );
+    db.IngredientRatings.findAll({
+      where: { ChemicalName: parsedingredient }
+    }).then(data => res.json(data));
   });
 };
